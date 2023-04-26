9f77c08bee39522eab985618ad89b0135ddc0e19
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-04-26 22:26:35 +0545

    Final Commit

---
+++ client.js
@@ -0,0 +1,37 @@
+const { fork } = require("child_process");
+const { connect } = require("net");
+
+function multiplexChannels(sources, destination) {
+  let openChannels = sources.length;
+
+  for (let i = 0; i < openChannels; i++) {
+    sources[i]
+      .on("readable", () => {
+        let chunk;
+        while ((chunk = sources[i].read()) !== null) {
+          // 1 for channel id , 4 for chunk length and remaining for actual data
+          console.log("chunk");
+          console.log(chunk);
+          const outBuffer = Buffer.alloc(1 + 4 + chunk.length);
+          outBuffer.writeUint8(i, 0);
+          outBuffer.writeUint32BE(chunk.length, 1);
+          chunk.copy(outBuffer, 5);
+          console.log(`Sending packet to channel ${i}`);
+          console.log("final Buffer");
+          console.log(outBuffer);
+
+          destination.write(outBuffer);
+        }
+      })
+      .on("end", () => {
+        if (--openChannels === 0) {
+          destination.end();
+        }
+      });
+  }
+}
+
+const socket = connect(3000, () => {
+  const child = fork(process.argv[2], process.argv.slice(3), { silent: 3 });
+  multiplexChannels([child.stdout, child.stderr], socket);
+});
